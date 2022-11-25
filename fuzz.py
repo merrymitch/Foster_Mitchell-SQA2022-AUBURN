8e643407ad378f57c10f7b2ae3ab5e8b4c7d212c
Mary <mem0250@auburn.edu>
2022-11-25 17:21:19 -0600

    Update fuzz.py

--- fuzz.py
+++ fuzz.py
@@ -1,1 +1,12 @@
+# Title:       Project for Software Quality Assurance (COMP 5710/6710)
+# Description: Automatically fuzzes five methods from TestOrchestrator4ML
+# Authors:     Mary Mitchell (mem0250) and Adia Foster (azf0046)
+# Course:      COMP5710 - Software Quality Assurance
+# Due Date:    1 December 2022
+# Run:         fuzz.py will be automatically executed from GitHub actions
 
+def main():
+    print("fuzz.py initial commit")
+
+if __name__ == "__main__":
+    main()
