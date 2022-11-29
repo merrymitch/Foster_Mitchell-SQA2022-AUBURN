57d4d03cf016c04f3e28e5eea0d4b5e29a728679
Mary <mem0250@auburn.edu>
2022-11-29 07:55:41 -0600

    Update fuzz.py

--- fuzz.py
+++ fuzz.py
@@ -5,8 +5,41 @@
 # Due Date:    1 December 2022
 # Run:         fuzz.py will be automatically executed from GitHub actions
 
+from TestOrchestrator4ML-main.detection.py_parser import checkMetricNames 
+from TestOrchestrator4ML-main.generation.py_parser import checkAlgoNames
+from TestOrchestrator4ML-main.generation.main import generateAttack
+from TestOrchestrator4ML-main.label_perturbation_attack.cliffsDelta import runs
+from TestOrchestrator4ML-main.label_perturbation_attack.knn import euc_dist
+
+def fuzzCheckMetricNames():
+    fuzzValues = [0, None, 'RANDOMSTRING', {'RANDOM': 'DICTIONARY'}, 3.14159, {None}, [None], b'0000', {}, []]
+    print("Start fuzzing checkMetricNames...")
+    for input in fuzzValues:
+        try: 
+            checkMetricNames(input)
+        except Exception as exct:
+            print("checkMetricNames failed with input: " + input)
+    print("Finish fuzzing checkMetricNames...")
+
+def fuzzCheckAlgoNames():
+    print("Start fuzzing checkAlgoNames...")
+    print("Finish fuzzing checkAlgoNames...")
+
+def fuzzGenerateAttack():
+    print("Start fuzzing generateAttack...")
+    print("Finish fuzzing generateAttack...")
+
+def fuzzRuns():
+    print("Start fuzzing runs...")
+    print("Finish fuzzing runs...")
+
+def fuzzEuc_Dist():
+    print("Start fuzzing euc_dist...")
+    print("Finish fuzzing euc_dist...")
+
 def main():
-    print("fuzz.py initial commit")
+    print("*** Begin Fuzzer ***")
+    print("*** End Fuzzer ***")
 
 if __name__ == "__main__":
     main()
