0a2ee46b1aa54f9d166d6ead55f3d327ba0d973a
Mary <mem0250@auburn.edu>
2022-11-29 21:56:23 -0600

    Create forensic_logging.py

---
+++ TestOrchestrator4ML-main/label_perturbation_attack/forensic_logging.py
@@ -0,0 +1,13 @@
+# Title:       Project for Software Quality Assurance (COMP 5710/6710)
+# Description: Logging method used to integrate forensics in various functions
+# Team:        Mary Mitchell (mem0250) and Adia Foster (azf0046)
+# Source:      This code comes from workshop 9 
+# Course:      COMP5710 - Software Quality Assurance
+# Due Date:    1 December 2022
+
+import logging
+
+def getLoggerObj():
+    logging.basicConfig(filename='../../FORENSIC_LOGS.LOG', level=logging.INFO, format='%(asctime)s:::%(name)s:::%(levelname)s:::%(message)s', datefmt='%d-%b-%y %H:%M:%S') 
+    loggerObj = logging.getLogger('forensic-logger') 
+    return loggerObj 
